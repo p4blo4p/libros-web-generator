b1cd38f3e41dc76c2e8e7738682e6b45702f5409
p4blo4p <87369479+p4blo4p@users.noreply.github.com>
2025-05-29 10:26:13 +0200

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,4 @@ app = create_app()
 if __name__ == '__main__':
     # No uses app.run() directamente en producción con este servidor de desarrollo.
     # Usa un servidor WSGI como Gunicorn o uWSGI.
-    app.run(debug=True) # debug=True es para desarrollo
-    
-    
-    
+    app.run(debug=True)  # debug=True es para desarrollo
